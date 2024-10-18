fd00854038c20c07ccd0012c8a9fb7a1e4847d8c
Joshua Lusk <jlusk@xplorie.com>
2024-10-18 07:44:16 -0400

    add entrypoint

--- setup.py
+++ setup.py
@@ -4,4 +4,7 @@ setup(
     name="no_set_literals",
     version="0.0.1",
     packages=["no_set_literals"],
+    entry_points={
+        "console_scripts": ["no-set-literals=no_set_literals.no_set_literals_hook:main"]
+    },
 )
